b743ae7ad196541ec9c64d1e6283c3edb109150d
Kunal-Sharma816 <142691986+Kunal-Sharma816@users.noreply.github.com>
2024-12-02 06:32:31 +0000

    conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,45 @@
+let score = 22
+// console.log(typeof score);
+
+// convert to string
+
+let valueinString = String(score)
+// console.log(valueinString);
+// console.log(typeof valueinString);
+
+// if we have string like this 
+
+let s  = '223kav'
+// convert to number
+let valueinNumber = Number(s)
+// console.log(typeof valueinNumber); // give number 
+// console.log(valueinNumber); // value is NaN 
+/*
+    "33" => 33
+    "22bajk" => NaN
+    true = 1
+    false = 0
+*/
+
+
+// for booleans
+
+let input = 8.1
+let booleaninput =  Boolean(input)
+
+console.log(booleaninput);
+
+
+/*
+    1 => true
+    0 => false
+    "" => false
+    "kuanl" => true
+    8.3 => true
+
+*/
+
+
+
+
+
