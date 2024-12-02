01d6297c88cdf239364298f8b8d79929176ebe3c
Kunal-Sharma816 <142691986+Kunal-Sharma816@users.noreply.github.com>
2024-12-02 05:55:57 +0000

    basics

---
+++ 01_basics/basics.js
@@ -0,0 +1,13 @@
+// console.log("hello kunal");
+
+const v = 1234;
+let ne = "kuanl";
+var n = '12332342';
+
+// v = 90; // in const variable whatever we write we cannot update it again
+ne = "sharma"
+n = '8902'
+
+console.table([v , ne , n])
+
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+// alert(2 + 7); it gives error because we are using nodejs not browser
+
+// DATA TYPES
+
+/*
+1. numbers => 2/8/9/1
+2. string => "hello"
+3. boolean => true/false
+4. null => standalone value
+5. undefined => 
+6. symbol => used for showing uniqness
+7. object 
+
+*/
+
+console.log(typeof "kunal"); // it gives u "what is the type of input "
+
