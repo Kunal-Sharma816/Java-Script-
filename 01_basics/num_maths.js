2f91a2d00afce3a50648c244209b527106cddab5
Kunal-Sharma816 <142691986+Kunal-Sharma816@users.noreply.github.com>
2024-12-03 10:47:04 +0000

    Num and Maths

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,37 @@
+
+// these are two ways to define number
+const score = 300
+// console.log(score);
+
+const balance = new Number(1000)
+// console.log(balance);
+
+
+// do some operation on number
+
+// console.log(balance.toString()); // convert to string
+
+// convert to some precise value like float
+
+// console.log(balance.toFixed(3));
+
+// use of precesion
+
+const num = 23.918
+
+
+// console.log(num.toPrecision(3));
+
+
+// ++++++++++++++ MATHS ++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(89.51))
+
+// random value
+
+console.log(Math.random())
+
+
